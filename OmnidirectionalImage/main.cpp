9458a9cea41d15715e26dbd21b697aada314ffdc
nakazawa9892 <nakazawa9892@yahoo.co.jp>
2015-12-27 21:37:33 +0900

    Initial Commit

---
+++ OmnidirectionalImage/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OmnidirectionalImage
+//
+//  Created by masakazu nakazawa on 2015/12/27.
+//  Copyright © 2015年 masakazu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
