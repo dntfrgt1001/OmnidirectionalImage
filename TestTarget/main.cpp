3334f793eac39888c9d972cacc4bfe82e0181320
nakazawa9892 <nakazawa9892@yahoo.co.jp>
2016-07-05 15:06:00 +0900

    20160705-6

---
+++ TestTarget/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  TestTarget
+//
+//  Created by masakazu nakazawa on 2016/07/05.
+//  Copyright © 2016年 masakazu. All rights reserved.
+//
+
+#include <iostream>
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
