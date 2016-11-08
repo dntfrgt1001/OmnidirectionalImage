3a955177c8932df0804f645d65ddcd1cb5027179
nakazawa9892 <masakazu@nakazawa-pro.local>
2016-11-08 15:17:00 +0900

    クラス書き換え前

---
+++ Core/Core.cpp
@@ -0,0 +1,36 @@
+//
+//  Core.cpp
+//  OmnidirectionalImage
+//
+//  Created by masakazu nakazawa on 2016/11/08.
+//  Copyright © 2016年 masakazu. All rights reserved.
+//
+
+#include "Core.hpp"
+
+#include <opencv2/core.hpp>
+
+class Equirect : public cv::Point2f
+{
+    
+};
+
+class Polar : public cv::Point2f
+{
+    
+};
+
+class Sphere : public cv::Point3f
+{
+    
+};
+
+class Normal : public cv::Point2f
+{
+    
+};
+
+class Pers : public cv::Point2f
+{
+    
+};
---
+++ Core/Core.hpp
@@ -0,0 +1,14 @@
+//
+//  Core.hpp
+//  OmnidirectionalImage
+//
+//  Created by masakazu nakazawa on 2016/11/08.
+//  Copyright © 2016年 masakazu. All rights reserved.
+//
+
+#ifndef Core_hpp
+#define Core_hpp
+
+#include <stdio.h>
+
+#endif /* Core_hpp */
