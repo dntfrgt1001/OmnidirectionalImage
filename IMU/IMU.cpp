5d6d3f4da6840d3218b8e55fe16e7f641bf5a938
nakazawa9892 <nakazawa9892@yahoo.co.jp>
2016-11-14 11:33:10 +0900

    20161114 IMU途中

--- IMU/IMU.cpp
+++ IMU/IMU.cpp
@@ -8,14 +8,63 @@
 
 #include "IMU.hpp"
 
-void IMU::initTermios(const std::string& port)
+IMU::IMU
+(const std::string& outPath, const std::string& port, const speed_t baudRate,
+ const size_t bufSize, const std::string& splPat):
+bufSize(bufSize), splPat(splPat)
+{
+    initTermios(port, baudRate);
+}
+
+IMU::~IMU()
+{
+    // 元のtermiosに戻す
+    tcsetattr(fd, TCSANOW, &terOld);
+}
+
+void IMU::initTermios(const std::string& port, const speed_t baudRate)
 {
     // ポートを開く
-    int fd = open(port.c_str(), O_RDONLY|O_NONBLOCK);
+    fd = open(port.c_str(), O_RDONLY|O_NONBLOCK);
     if (fd < 0) {
         std::cout << "port cannot be opened" << std::endl;
         exit(1);
     }
     
+    // 古いtermiosを保存
+    tcgetattr(fd, &terOld);
     
+    // termios構造体をゼロで初期化
+    memset(&terNew, 0, sizeof(termios));
+    // 文字サイズを設定
+    terNew.c_cflag = CS8;
+    // 入出力速度設定
+    cfsetspeed(&terNew, baudRate);
+    // パラメータ設定
+    tcsetattr(fd, TCSANOW, &terNew);
+}
+
+void IMU::inputData()
+{
+    // ポートからデータを読むためのバッファ
+    char inputBuf[bufSize];
+    
+    // データの読み込み
+    int inputLen = read(fd, inputBuf, bufSize);
+    
+    // 文字列として扱うために終端文字を追加
+    inputBuf[inputLen] = '\0';
+    
+    std::string inputString;
+    
+}
+
+void IMU::charInput2strInput
+(const char *charIn, const size_t size, std::string &strIn)
+{
+    strIn = "";
+    
+    for (int i = 0; i < size; i++) {
+        <#statements#>
+    }
 }
--- IMU/IMU.hpp
+++ IMU/IMU.hpp
@@ -20,13 +20,25 @@ class  IMU
 public:
     IMU(const std::string& outPath, const std::string& port,
         const speed_t baudRate, const size_t bufSize,
-        const std::string& splpat);
+        const std::string& splPat);
+    ~IMU();
     
     // termios構造体の初期化
-    void initTermios(const std::string& port);
+    void initTermios(const std::string& port, const speed_t baudRate);
+    
+    // センサからデータを入力
+    void inputData();
+    
+    // char型のセンサデータをstring型に変換
+    void charInput2strInput
+    (const char* charIn, const size_t size, std::string& strIn);
     
 private:
+    int fd;
     const size_t bufSize;
+    termios terNew;
+    termios terOld;
+    const std::string splPat;
     
 };
 
